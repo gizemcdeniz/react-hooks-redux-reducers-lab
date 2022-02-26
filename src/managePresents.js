7a6f2d45fcce70a110db3ec46054a4215b8155ae
Akin <akinkavak1@gmail.com>
2022-02-26 03:35:40 +0300

    Adding recuders

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,10 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return {...state, numberOfPresents: initialState.numberOfPresents + 1 }
+    default:
+      return state;
+  }
 }
